7de6ce03b541aba289c6ca4a595fa11f68768dac
Saulo Camposi <saulocamposi@gmail.com>
2016-09-22 15:22:20 -0300

    Update products resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316021134) do
+ActiveRecord::Schema.define(version: 20160922171245) do
 
   create_table "addresses", force: true do |t|
     t.string   "city"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20160316021134) do
     t.string   "origem"
     t.string   "destino"
     t.string   "eficiencia"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
