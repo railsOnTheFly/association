9b6d77997d87318b278b30437598f6703d9e9e17
Saulo Camposi <saulocamposi@gmail.com>
2016-10-04 11:11:53 -0300

    Add product_test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     puts "Assert True Product model test assert true with message before"
+     assert true
+   end
 end
