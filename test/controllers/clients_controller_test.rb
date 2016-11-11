2a0f4485cd44cfa6a79e21255cc68ba2ca0c283e
Saulo Camposi <saulocamposi@gmail.com>
2016-11-11 20:06:36 -0200

    Update test

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -1,24 +1,14 @@
 require 'test_helper'
 
 class ClientsControllerTest < ActionController::TestCase
+  setup do
+    @client = clients(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
-  end
-
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
-    assert_response :success
+    assert_not_nil assigns(:clients)
   end
 
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-   test "the truth" do
-     puts "Assert True Product model test assert true with message before"
-     assert true
-   end
+  # test "the truth" do
+  #   assert true
+  # end
 end
