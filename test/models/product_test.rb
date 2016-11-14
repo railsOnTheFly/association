592ec268f9b0312b6903c65b41291916c8574f4f
Saulo Camposi <saulocamposi@gmail.com>
2016-11-14 19:55:52 -0200

    Update test,implementation has many orders

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "has many orders" do
+     product = Product.new
+     assert product.orders
+   end
 end
